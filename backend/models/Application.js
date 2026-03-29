4d27ce5bbb5c80280cb48d5ea076e90ed1a89a11
Videesha A G <videeshaag@gmail.com>
2026-03-29 11:10:21 +0530

    Add files via upload
    initial project upload

---
+++ backend/models/Application.js
@@ -0,0 +1,9 @@
+const mongoose = require("mongoose")
+
+const appSchema = new mongoose.Schema({
+  user_id: String,
+  job_id: String,
+  status: String
+})
+
+module.exports = mongoose.model("Application", appSchema)
